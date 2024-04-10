3b6593743659ea16df9a9c592c471dde9a071cac
MrDerpus <47982738+MrDerpus@users.noreply.github.com>
2024-04-10 23:34:40 +1000

    v0.0.2
    Added:
    + Comments
    	> Single line comments that are only used with in the SHTML script can be defined by '/'.
    	> Single line comments that are used in both files can be defined by '//'.
    
    + Commands
    	> $close only exists to close div tags.  This will be removed in later versions.
    
    + Tags
    	> h1, h2, h3, p, br & div
    
    
    What I want to add next:
    	+ Cleaner code.
    	+ Better comments.
    	+ Multiline comments.
    	+ More tags.
    	+ More commands.
    	- Remove $close command.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Author: MrDerpus
 
 
-Version: 0.0.1
+Version: 0.0.2
 
 
 Dev conditions:
@@ -16,21 +16,42 @@ Fedora  38
 
 
 Description: 
-Simple HTML (SHTML)
+Simple HTML (SHTML).
+
+
+Added:
++ Comments
+	> Single line comments that are only used with in the SHTML script can be defined by '/'.
+	> Single line comments that are used in both files can be defined by '//'.
+
++ Commands
+	> $close only exists to close div tags.  This will be removed in later versions.
+
++ Tags
+	> h1, h2, h3, p, br & div
+
+
+What I want to add next:
+	+ Cleaner code.
+	+ Better comments.
+	+ Multiline comments.
+	+ More tags.
+	+ More commands.
+	- Remove $close command.
 '''
 
 
 
 
-from rich.traceback import install; install(show_locals = True) # type: ignore
-from rich.console   import Console; c = Console() # type: ignore
+from rich.traceback import install; install(show_locals = True)
+from rich.console   import Console; c = Console()
 
-import click # type: ignore
+import click
 
 import sys
 import os
 
-from settings import HASH
+from settings import HASH, COMMENTS
 
 
 # custom function to test if a string is wrapped in a specific char
@@ -47,31 +68,32 @@ def cli():
 
 
 
-# python3 main.py convert_to_html --input_file 'script.txt' --output_file 'index.html'
-# python3 main.py build --input_file 'script.txt' --output_file 'index.html'
-# shtml build --input_file 'script.txt' --output_file 'index.html'
-# shtml build -i 'script.txt' -o 'index.html'
 
 @click.command()
-@click.option('--input_file',  '-i',  default = HASH.NO_INPUT_FILE)
-@click.option('--output_file', '-o',  default = HASH.NO_OUTPUT_FILE)
-@click.option('--error_debug', '-d0', default = False)
-def build(input_file:str, output_file:str, error_debug:bool):
+@click.option('--input_file',   '-i',  default = HASH.NO_INPUT_FILE)
+@click.option('--output_file',  '-o',  default = HASH.NO_OUTPUT_FILE)
+@click.option('--minimal_html', '-m',  default = False)
+@click.option('--error_debug', '-d0',  default = False)
+def build(input_file:str, output_file:str, minimal_html:bool, error_debug:bool):
 
-	# the separator character that can be changed using the $sep command.
-	syntax_sep = '|'
-	innerText = ''
-	tag_args = ''
-	kill = 0
-	line_count = 1
+	# Var declare 
+	syntax_sep:str = '|' # the separator character that can be changed using the $sep command.
+	converted_line:str = ''
+	innerText:str = ''
+	tag_args:str = ''
+	kill:int = 0
+	line_count:str = 1
+	keyword:str = ''
+	previous_keyword:str = ''
+	minimal_file_output:bool = minimal_html
 	
 	
 	if(input_file == HASH.NO_INPUT_FILE):
-		c.print(' There was no input SHTML script defined. \n', style = '#ff0000')
-		sys.exit(0)
+		c.print(' There was no input .SHTML script defined. \n', style = '#ff0000')
+		sys.exit(1)
 	elif(output_file == HASH.NO_OUTPUT_FILE):
-		c.print(' There was no output HTML file defined.   \n', style = '#ff0000')
-		sys.exit(0)
+		c.print(' There was no output .HTML file defined.   \n', style = '#ff0000')
+		sys.exit(1)
 	
 	
 	
@@ -79,7 +101,7 @@ def build(input_file:str, output_file:str, error_debug:bool):
 	if(os.path.exists(output_file)):
 		os.remove(output_file)
 
-	c.print(f' Building output: {output_file}', style = '#ffff00')
+	c.print(f' Building output: {output_file}', style = '#00ffff')
 	
 	# Read file line by line
 	with open(input_file, 'r') as infile:
@@ -92,10 +114,10 @@ def build(input_file:str, output_file:str, error_debug:bool):
 			keyword = line.split('"')[0].lstrip().rstrip().lower()
 
 
-
 			# Grab tag innerText
 			try:
 				innerText = line.split('"')[1]
+				innerText = innerText.replace("/n", '<br>')
 			except:
 				innerText = ''
 
@@ -112,6 +134,25 @@ def build(input_file:str, output_file:str, error_debug:bool):
 
 
 
+			# is line a comment?
+			comment = len(line) >= 1
+			if(comment and line[0] == COMMENTS.SINGLE_LINE_SCRIPT):   # comment for script itself.
+				keyword = HASH.COMMENT
+				converted_line = ''
+			
+			if(comment and line[0:2] == COMMENTS.SINGLE_LINE_HTML): # html comment
+				keyword = HASH.COMMENT
+				innerText = line[2:].lstrip().rstrip()
+				converted_line = f'<!-- {innerText} -->'
+
+
+
+			#print(f'{line_count=}\n{keyword=}\n{comment=}\n{innerText=}\n\n')
+
+
+			#converted_line = f'<{keyword} {tag_args}>{innerText}</>'
+
+			#sys.exit(f' DEBUG: better line formatting test!\n {converted_line}')
 
 			# keywords and commands
 			match keyword:
@@ -133,11 +174,32 @@ def build(input_file:str, output_file:str, error_debug:bool):
 				case 'body':
 					converted_line = f'</head>\n<body {tag_args}>'
 
-				case str(HASH.IGNORE): # ignore line
+				case 'br':
+					converted_line = '<br>'
+
+				case str(HASH.COMMENT): # ignore line
 					pass
+
 
 				case 'h1':
 					converted_line = f'<h1 {tag_args}>{innerText}</h1>'
+				
+				case 'h2':
+					converted_line = f'<h2 {tag_args}>{innerText}</h2>'
+
+				case 'h3':
+					converted_line = f'<h3 {tag_args}>{innerText}</h3>'
+
+				case 'p':
+					converted_line = f'<p {tag_args}>{innerText}</p>'
+
+				case 'div':
+					converted_line = f'<div {tag_args}>'
+					previous_keyword = keyword
+
+
+				case '$close':
+					converted_line = f'</{previous_keyword}>'
 
 				case '$exit':
 					converted_line = '</body>\n</html>'
@@ -147,22 +209,31 @@ def build(input_file:str, output_file:str, error_debug:bool):
 					converted_line = ''
 					syntax_sep = line.split('"')[1]
 
+				case '$min':
+					minimal_file_output = True
+
 				case _:
-					#print(line.split())
-					converted_line = ''
+					if(len(keyword) > 0):
+						c.print(f' Unknown keyword @ line {line_count}: {keyword}. ', style = '#ffff00')
+						converted_line = f'<!-- Unknown keyword: {keyword} -->'
+					else:
+						converted_line = ''
 
 
 
 			#output to output_file
-			with open(output_file, 'a') as outfile:
-				outfile.write(converted_line + '\n')
+			if(len(converted_line) > 0): # only print output if changes are made.
+				with open(output_file, 'a') as outfile:
+					outfile.write(converted_line)
+					if(minimal_file_output == False):
+						outfile.write('\n')
 
 			
 			line_count += 1
 
 			if kill >= 1:
 				#c.print(' Exiting . . .', style = '#ffff00')
-				sys.exit()
+				sys.exit(0)
 
 
 
--- settings.py
+++ settings.py
@@ -1,8 +1,29 @@
+
+class COMMENTS:
+	SINGLE_LINE_SCRIPT:str = '/'
+	SINGLE_LINE_HTML:str   = '//'
+
+	MULTI_LINE_START:str = '!-'
+	MULTI_LINE_END:str   = '-!'
+	Multiline = False
+
+
+
+
+
 class HASH:
 	
+
+	
+
+
 	NO_INPUT_FILE:str  = '509bb0bcae3e762ece6891ce4d34a841'
 	NO_OUTPUT_FILE:str = 'f4d6cc0dc0dd99d177b8253bf1e43c6f'
 	NO_USER_INPUT:str  = '75e2c40ffa5b937511e96840b56a4a7a'
 
-	COMMENT:str = 'f2cd320b55767434dd48d81b165ea956'
 	IGNORE:str  = 'a2e843feab94ef623fea888f07c28696'
+
+	COMMENT:str = 'f2cd320b55767434dd48d81b165ea956'
+	MULTI_LINE_COMMENT_START:str = '741e8dedeff3fda6a9183224ad3eab44'
+	MULTI_LINE_COMMENT_END:str   = '11672c1086f8b351a09d6bbcc8915e43'
+	
