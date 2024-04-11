65619ef5a37ecdaf540b8555418e10e79b485d84
MrDerpus <47982738+MrDerpus@users.noreply.github.com>
2024-04-12 06:58:22 +1000

    0.0.3 - A
    Will work fine, but no documentation yet, wait for '0.0.3'

--- settings.py
+++ settings.py
@@ -3,20 +3,21 @@ class COMMENTS:
 	SINGLE_LINE_SCRIPT:str = '/'
 	SINGLE_LINE_HTML:str   = '//'
 
+'''	
 	MULTI_LINE_START:str = '!-'
 	MULTI_LINE_END:str   = '-!'
 	Multiline = False
+'''
 
 
 
+class SYNTAX: # syntax settings that users can change.
+	SEPARATOR:str = '|'
+	CLOSE:str = '!'
 
 
-class HASH:
-	
 
-	
-
-
+class HASH: # Hashes, because it's very unlikely for a person to type in a full hash.
 	NO_INPUT_FILE:str  = '509bb0bcae3e762ece6891ce4d34a841'
 	NO_OUTPUT_FILE:str = 'f4d6cc0dc0dd99d177b8253bf1e43c6f'
 	NO_USER_INPUT:str  = '75e2c40ffa5b937511e96840b56a4a7a'
@@ -26,4 +27,3 @@ class HASH:
 	COMMENT:str = 'f2cd320b55767434dd48d81b165ea956'
 	MULTI_LINE_COMMENT_START:str = '741e8dedeff3fda6a9183224ad3eab44'
 	MULTI_LINE_COMMENT_END:str   = '11672c1086f8b351a09d6bbcc8915e43'
-	
